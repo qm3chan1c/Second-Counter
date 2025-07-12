66b884ef37bb5ff08a6eafe4757cd9ed57b7c939
qm3chan1c <82043721+qm3chan1c@users.noreply.github.com>
2025-07-13 00:57:32 +0530

    Update clock_divider.v
    Changed the functionality of the code by generating a clock enable output signal which is later used by number_counter.v
    
    This was done to address potential timing violations in the design due to the (unnnecessary/undesirable) creation of a new clock domain from the original 100MHz clock signal.

--- sources/clock_divider.v
+++ sources/clock_divider.v
@@ -1,15 +1,15 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: --
-// Engineer: QJ
+// Company: 
+// Engineer: 
 // 
 // Create Date: 07/06/2025 08:56:51 PM
-// Design Name: Clock Divider
+// Design Name: 
 // Module Name: clock_divider
-// Project Name: Second-Counter
-// Target Devices: Basys 3
-// Tool Versions: Vivado 2024.2
-// Description: Divides a 100MHz clock to a 1Hz clock
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
 // 
 // Dependencies: 
 // 
@@ -22,7 +22,7 @@
 
 module clock_divider(
     input clk_in,
-    output reg divided_clk_out
+    output reg enable_clk_out
     );
     
 
@@ -33,16 +33,17 @@ module clock_divider(
         
         
         
-        if (counter_signal < 32'b00000010111110101111000010000000) begin
+        if (counter_signal < 32'd99999999) begin
             counter_signal <= counter_signal + 1;
-            divided_clk_out <= 1'b0;
+            enable_clk_out <= 1'b0;
         
-        end else if ((counter_signal < 32'b00000101111101011110000100000000) & (counter_signal >= 32'b00000010111110101111000010000000)) begin
-            counter_signal <= counter_signal + 1;
-            divided_clk_out <= 1'b1;
+//        end else if ((counter_signal < 32'b00000101111101011110000100000000) & (counter_signal >= 32'b00000010111110101111000010000000)) begin
+//            counter_signal <= counter_signal + 1;
+//            divided_clk_out <= 1'b1;
         
         end else begin
             counter_signal <= {32{1'b0}};
+            enable_clk_out <= 1'b1;
         end
     end
 endmodule
